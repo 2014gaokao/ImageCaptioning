16e5fd3048b7bf12ee0418fa1397d37835f10115
2014gaokao <2225052023@qq.com>
2020-04-25 22:05:16 +0800

    second commit

---
+++ test.sh
@@ -0,0 +1,1 @@
+python eval.py --model log/log_aoanet_rl/model.pth --infos_path log/log_aoanet_rl/infos_aoanet.pkl  --dump_images 0 --dump_json 1 --num_images -1 --language_eval 1 --beam_size 2 --batch_size 100 --split test
